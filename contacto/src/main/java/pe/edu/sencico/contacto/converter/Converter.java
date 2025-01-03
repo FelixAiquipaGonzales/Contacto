8a9db859eff71860c06bfb1b4f5c90258e9ff310
FELIX ALDIMAR AIQUIPA GONZALES <aiquipafelix@gmail.com>
2025-01-03 10:37:05 -0500

    se procedio a crear el converter de entity y VO v-01

--- contacto/src/main/java/pe/edu/sencico/contacto/converter/Converter.java
+++ contacto/src/main/java/pe/edu/sencico/contacto/converter/Converter.java
@@ -1,10 +1,10 @@
 package pe.edu.sencico.contacto.converter;
-
+//se procede a invocar las clases entity y contactoVO
 import pe.edu.sencico.contacto.entity.Contacto;
 import pe.edu.sencico.contacto.vo.ContactoVO;
-
+//se creo una interfaz para 
 public interface Converter {
-
+    //definimos los metodos converToVo y ConvertToEntity.
 	ContactoVO convertToVo(Contacto contacto);
     
     Contacto convertToEntity(ContactoVO contactoVO);
